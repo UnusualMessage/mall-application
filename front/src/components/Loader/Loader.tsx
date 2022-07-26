3dfdf7203a0d59259c581348709cb3eaeefabde0
UnusualMessage <nikita_kot2@mail.ru>
2022-07-26 17:04:17 +0300

    Improve adaptibility

--- front/src/components/Loader/Loader.tsx
+++ front/src/components/Loader/Loader.tsx
@@ -7,7 +7,6 @@ const Loader = () => {
 				xmlns="http://www.w3.org/2000/svg"
 				className={`${css.default}`}
 				viewBox="0 0 270.93 270.93"
-				style={{gridColumn: "1 / 2", gridRow: "1 / 2"}}
 			>
 				<path fill="#00000000" strokeWidth="0.07" d="M0 0H270.93V270.93H0z"></path>
 			</svg>
