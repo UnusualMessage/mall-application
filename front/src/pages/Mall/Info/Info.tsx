433f050643a55ef9d249feff1779f0f8a2703928
UnusualMessage <nikita_kot2@mail.ru>
2022-07-29 14:48:49 +0300

    Add Loader for Yandex Map

--- front/src/pages/Mall/Info/Info.tsx
+++ front/src/pages/Mall/Info/Info.tsx
@@ -6,15 +6,33 @@ import label from "/src/components/Label/label.module.scss";
 import Label from "../../../components/Label";
 
 import contacts from "../../../data/contacts";
+import Loader from "../../../components/Loader";
+import {useState} from "react";
 
 const Info = () => {
+	const [loaded, setLoaded] = useState(false);
+	
+	const onMapLoad = () => {
+		setLoaded(true);
+	};
+	
+	const markerPosition = [54.35079996381128,38.261544220237724];
+	
 	return(
 		<div className={`${css.wrapper}`}>
-			<YMaps>
-				<Map style={{ width: "100%", height: "700px" }} defaultState={{ center: [54.35079996381128,38.261544220237724], zoom: 17 }}>
-					<Placemark defaultGeometry={[54.35079996381128,38.261544220237724]}/>
-				</Map>
-			</YMaps>
+			<div style={{ width: "100%", height: "700px"}}>
+				{ loaded ? <></> : <Loader/>}
+				
+				<YMaps>
+					<Map style={{ width: "100%", height: "100%" }}
+					     defaultState={{ center: markerPosition, zoom: 17 }}
+					     onLoad={onMapLoad}>
+						
+						<Placemark defaultGeometry={markerPosition}/>
+					</Map>
+				</YMaps>
+			</div>
+			
 
 			<div className={`${css.about}`}>
 				<div className={`${css.info}`}>
